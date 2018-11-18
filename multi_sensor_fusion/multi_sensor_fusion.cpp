20f1c9a54bc51ef55a66acc5c3f7ca28215fcc44
Wangxin Liu <flybird1987@gmail.com>
2018-11-18 13:01:09 +0100

    motion only ba can work

--- multi_sensor_fusion/multi_sensor_fusion.cpp
+++ multi_sensor_fusion/multi_sensor_fusion.cpp
@@ -70,16 +70,18 @@ vector< CameraPose > generate_camera_poses()
 
 vector< Vector3d > generate_points_cloud()
 {
-    double unit = 3.0f*3.141592653/180;
-    double curr_angle = 0.0;
+    Vector3d p1{-3,-3,5};
+    Vector3d p2{-3,3,5};
+    Vector3d p3{3,-3,5};
+    Vector3d p4{3,3,5};
+    Vector3d interval{0,0,1};
     vector< Vector3d > points{};
     for(int i=0;i<10;i++)
     {
-       points.push_back(Vector3d{2*cos(curr_angle),2*sin(curr_angle),10});
-        /*points.push_back(Vector3d((uniform()-0.5)*3,
-                                  uniform()-0.5,
-                                  uniform()+3));*/
-        curr_angle += unit;
+       points.push_back(p1+i*interval);
+       points.push_back(p2+i*interval);
+       points.push_back(p3+i*interval);
+       points.push_back(p4+i*interval);
     }
     return points;
 }
@@ -87,7 +89,7 @@ vector< Vector3d > generate_points_cloud()
 
 void test_projection()
 {
-    double focal_length= 1000.;
+    double focal_length= 500.;
     Vector2d principal_point(320., 240.);
 
     SE3Quat pose{MatrixXd::Identity(3,3),Vector3d{0.0,0.0,0.0}};
@@ -133,7 +135,7 @@ int main()
     //test_projection();
     auto poses = generate_camera_poses();
     auto points = generate_points_cloud();
-    bool DENSE = false;
+    bool DENSE = true;
 
     g2o::SparseOptimizer optimizer;
     optimizer.setVerbose(false);
@@ -150,7 +152,7 @@ int main()
 
     optimizer.setAlgorithm(solver);
 
-    double focal_length= 1000.;
+    double focal_length= 300.;
     Vector2d principal_point(320., 240.);
 
     g2o::CameraParameters * cam_params
@@ -161,29 +163,35 @@ int main()
       assert(false);
     }
 
+
+
     vector<g2o::VertexSE3Expmap*> se3_vertices;
+    map<g2o::VertexSE3Expmap*,g2o::SE3Quat> groundTruth;
     int vertex_id = 0;
     for(auto pose:poses)
     {//Add the pose vertices
-        g2o::SE3Quat curr_pose(pose.R,pose.t);
+        g2o::SE3Quat curr_pose(pose.R,pose.t);        
         g2o::VertexSE3Expmap* vertex = new g2o::VertexSE3Expmap();
         vertex->setId(vertex_id);        
         curr_pose.setTranslation(curr_pose.translation());
 
 //        curr_pose.setTranslation(curr_pose.translation()+Vector3d(uniform(),
 //                                                                  uniform(),
-//                                                                  uniform()));
-        vertex->setEstimate(curr_pose);                      
+//                                                                  uniform()));        
+        groundTruth[vertex] = curr_pose;
+        g2o::SE3Quat noisyPose = curr_pose;
+        noisyPose.setTranslation( curr_pose.translation() + 3.0*Vector3d(uniform(),uniform(),uniform()) );
+        vertex->setEstimate(noisyPose);
         vertex->setFixed(false);
         se3_vertices.push_back(vertex);
         optimizer.addVertex(vertex);
         vertex_id++;
 
-//        g2o::EdgeSE3ExpXYZPointPrior* gps_constrains = new g2o::EdgeSE3ExpXYZPointPrior();
-//        gps_constrains->setVertex(0, dynamic_cast<g2o::OptimizableGraph::Vertex*>(vertex));
-//        gps_constrains->setMeasurement(pose.t);
-//        gps_constrains->setInformation(Matrix3d::Identity()*2);
-//        optimizer.addEdge(gps_constrains);
+        g2o::EdgeSE3ExpXYZPointPrior* gps_constrains = new g2o::EdgeSE3ExpXYZPointPrior();
+        gps_constrains->setVertex(0, dynamic_cast<g2o::OptimizableGraph::Vertex*>(vertex));
+        gps_constrains->setMeasurement(pose.t);
+        gps_constrains->setInformation(Matrix3d::Identity()*1000);
+        optimizer.addEdge(gps_constrains);
     }
 
     map<g2o::VertexSE3Expmap*,Vector2d> vertex_connections;
@@ -193,19 +201,18 @@ int main()
         g2o::VertexSBAPointXYZ* X_Vertex = new g2o::VertexSBAPointXYZ();
         X_Vertex->setId(vertex_id);
         X_Vertex->setMarginalized(true);
+        X_Vertex->setFixed(true);
         //Points initialization
-        X_Vertex->setEstimate(point + 0.5f*Vector3d(uniform(),
-                                                    uniform(),
-                                                    uniform()));
+        X_Vertex->setEstimate(point);
 
         vertex_connections.clear();
         for(auto pose_vertex:se3_vertices)
         {
-            Vector2d image_pixel = cam_params->cam_map(pose_vertex->estimate().map(point));
+            Vector2d image_pixel = cam_params->cam_map(groundTruth[pose_vertex].map(point));
             if (image_pixel[0]>=0 && image_pixel[1]>=0 && image_pixel[0]<640 && image_pixel[1]<480)
             {
-                vertex_connections[pose_vertex][0] = image_pixel[0];
-                vertex_connections[pose_vertex][1] = image_pixel[1];
+                vertex_connections[pose_vertex][0] = image_pixel[0] + 10*uniform();
+                vertex_connections[pose_vertex][1] = image_pixel[1] + 10*uniform();
             }
         }
 
@@ -234,7 +241,6 @@ int main()
 
     double before = compute_error(optimizer,estimation_gt);
     double pose_before = compute_pose_error(se3_vertices,poses);
-
 
     optimizer.initializeOptimization();
     optimizer.setVerbose(true);
